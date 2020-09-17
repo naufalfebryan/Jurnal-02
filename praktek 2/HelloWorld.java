b9a75ea78bdaf67e6821592891d074a866d604e1
Naufal Febryan <febryannaufal@student.telkomuniversity.ac.id>
2020-09-17 14:41:44 +0700

    HelloWorld.java

---
+++ praktek 2/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	    System.out.println("NAMA  : MUHAMMAD NAUFAL FEBRYAN");
+		System.out.println("NIM   : 6706202109");
+		System.out.println("KELAS : D3IF 44 O3");
+	}
+}
+	
